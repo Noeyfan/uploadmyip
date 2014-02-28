289494f1227aedd40103336c9da6f1beb1cbc732
Noeyfan <youfan.noey@gmail.com>
2014-02-28 22:27:48 +0000

    update

---
+++ upload.rb
@@ -0,0 +1,12 @@
+#! usr/bin/ruby 
+command = system('ifconfig | grep inet > add')
+File.open('add', 'r+') do |f|
+    #puts f.read
+    a = []
+    a<<f.read.split()
+    f.write(a.flatten[4])
+    f.close
+end
+
+command = system('git add . ; git commit -m "new ip";
+		git push')
